ced8ca27f8366acc1afe9b6be24c3e559cfbf6fd
Sander Mertens <sander.mertens8@gmail.com>
2021-11-07 20:07:39 -0800

    UI improvements & flecs update

--- query_explorer/js/query.js
+++ query_explorer/js/query.js
@@ -39,6 +39,27 @@ Vue.component('query', {
     },
     evt_close() {
       this.set_query();
+    },
+    result_count() {
+      return this.$refs.results.count();
+    }
+  },
+  computed: {
+    count: function() {
+      if (!this.query_result) {
+        return 0;
+      }
+
+      let result = 0;
+      for (let i = 0; i < this.query_result.results.length; i ++) {
+        const elem = this.query_result.results[i];
+        if (elem.entities) {
+          result += elem.entities.length;
+        } else {
+          result ++;
+        }
+      }
+      return result;
     }
   },
   template: `
@@ -52,11 +73,13 @@ Vue.component('query', {
         Query&nbsp;&nbsp;<query-editor
           ref="editor"
           :error="query_error"
-          v-on:changed="evt_query_changed"/>
+          v-on:changed="evt_query_changed"/>&nbsp;
+          <span class="query-result-count" v-if="count">({{count}}&nbsp;results)</span>
       </template>
 
       <template v-slot:detail>
         <query-results 
+          ref="results"
           :data="query_result" 
           :valid="valid && query_error === undefined"
           v-on="$listeners"/>
