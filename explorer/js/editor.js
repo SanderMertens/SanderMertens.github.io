53bbd26a44dbf29b634364dde38ad9359f9fcf0f
Sander Mertens <sander.mertens8@gmail.com>
2021-11-24 23:18:43 -0800

    Editor UI updates

--- explorer/js/editor.js
+++ explorer/js/editor.js
@@ -16,24 +16,22 @@ Vue.component('editor-textarea', {
       fence: false
     });
 
-    var ldtbehave = this.ldt;
 
     // Behavejs' event listeners  override some of LDT's, so we use Behavehooks to hook LDT updating to Behavejs' event listeners.
-    BehaveHooks.add('keydown', function(data){
-      setTimeout(function(){
-        ldtbehave.update();
-      })
-    });
-    BehaveHooks.add('keyup', function(data){
-      setTimeout(function(){
-        ldtbehave.update();
-      })
-    });
-    BehaveHooks.add('keypress', function(data){
-      setTimeout(function(){
-        ldtbehave.update();
-      })
-    });
+    // BehaveHooks are only used when doc query commands are unsupported, to force ldt update.
+    if (!document.queryCommandSupported('insertText')) {
+      BehaveHooks.add('keydown', (data) => {
+        setTimeout(() => {
+          this.ldt.update();
+        })
+      });
+      BehaveHooks.add('keypress', (data) => {
+        setTimeout(function(){
+          this.ldt.update();
+        })
+      });
+    }
+
   },
   updated: function() {
     this.ldt.update();
@@ -41,7 +39,34 @@ Vue.component('editor-textarea', {
   methods: {
     run() {      
       if (this.code != this.last_code) {
-        this.$emit('run-code', this.code);
+
+        if(this.last_key_event) 
+        {
+          this.key_events_since_emit++;
+          this.last_key_event = Date.now();
+
+          var callback_time = 600;
+          if (this.key_events_since_emit == 1) {
+            // Make single, unchained key events feel fast
+            callback_time = 250;
+          }
+
+          setTimeout(() => {
+            var time_delta = Date.now() - this.last_key_event;
+            if (
+              time_delta >= 600
+              || this.key_events_since_emit == 1 && time_delta >= 250
+              ) {
+              this.$emit('run-code', this.code); 
+              this.key_events_since_emit = 0;
+            }
+          }, callback_time);
+        } else {
+          // First code set
+
+          this.last_key_event = Date.now();
+          this.$emit('run-code', this.code); 
+        }
         this.last_code = this.code;
       }
     },
@@ -57,7 +82,9 @@ Vue.component('editor-textarea', {
   data: function() {
     return {
       code: undefined,
-      last_code: undefined
+      last_code: undefined,
+      last_key_event: undefined,
+      key_events_since_emit: 0,
     }
   },
   computed: {
